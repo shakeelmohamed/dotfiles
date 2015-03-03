840380553a01e3a6ac04e5dc1d3d607efc1e7b7a
Shakeel Mohamed <smohamed@splunk.com>
2015-03-02 17:30:50 -0800

    update shakshock

--- mac/shak.sh
+++ mac/shak.sh
@@ -1,5 +1,8 @@
 echo "Shak shock."
 
+# Edit this file
+alias shakshock="subl ~/shak.sh"
+
 # alias for cd-ing to git dir
 alias gitgit="cd ~/work/git"
 
@@ -13,7 +16,7 @@ export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home"
 alias splunks="cd ~/work/splunks"
 
 SPLUNK_HOME_BASE=~/work/splunks/splunk
-SPLUNK_VERSION_STR="622"
+SPLUNK_VERSION_STR="613"
 
 export SPLUNK_HOME=$SPLUNK_HOME_BASE$SPLUNK_VERSION_STR
 
@@ -25,3 +28,24 @@ alias SPLUNK=SPLUNKCMD
 
 # Sublime text!
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+
+# Android things
+export ANDROID_HOME="/Users/smohamed/Android"
+export PATH=$PATH:$ANDROID_HOME"/tools":$ANDROID_HOME"/platform-tools"
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PYENV_ROOT:$PATH
+
+if which pyenv > /dev/null;
+    then eval "$(pyenv init -)";
+fi
+
+# Splunk SDK release
+SDKRELCMD() {
+    md5 $1
+    openssl dgst -sha512 $1
+}
+alias SDKREL=SDKRELCMD
+
+
